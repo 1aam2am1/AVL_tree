7b5753b586dac030ad5474505d9c4dbd3ed0c4dd
Michal Marszalek <1aam2am1@gmail.com>
2018-03-19 17:40:46 +0100

    Change main to function interacting with user.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,50 @@
 
 #include "AVL_tree.h"
 
+inline std::string wypisz(std::string &str, const char &znak = ' ') {
+    std::string result;
+
+    std::size_t found = str.find(znak); //pierwszy znak liczac od 0
+
+    if (found != std::string::npos) {
+        result.insert(0, str, 0, found); //na pozycji 0 umieszcza str od 0(liczy od 0) do found(liczy od 1)
+        str.erase(0, found + 1);
+    } else {
+        result = str;
+        str.clear();
+    }
+
+    return result;
+}
+
 int main() {
     AVL_tree<int, int> m;
 
-    for (int32_t i = 0; i < 20; ++i) {
-        m[i] = i;
-    }
+    while (true) {
+        char tab[255];
 
-    m.print();
+        scanf("%254[^\n]%*c", tab);
+        std::string str = tab;
 
-    for (auto i : m) {
-        printf("%i ", i.first);
-    }
+        std::string command;
 
-    for (int32_t i = 2000; i > 500; i -= 3) {
-        m.erase(i);
+        command = wypisz(str);
+
+        if (command == "e") {
+            break;
+        } else if (command == "a") {
+            int i = atoi(str.c_str());
+
+            m[i] = 0;
+        } else if (command == "r") {
+            int i = atoi(str.c_str());
+
+            m.erase(i);
+        } else if (command == "p") {
+            m.print();
+        } else if (command == "h") {
+            printf("%i\n", m.height());
+        }
     }
 
 
